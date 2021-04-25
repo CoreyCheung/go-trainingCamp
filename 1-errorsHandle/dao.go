df30783a009614174c8cdc170a2525110fa7e637
corey_zhang <nicholouszhang@163.com>
2021-04-25 08:35:58 +0800

    error commit

---
+++ 1-errorsHandle/dao.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/jinzhu/gorm"
+
+)
+
+//1. 我们在数据库操作的时候，比如 dao 层中当遇到一个 sql.ErrNoRows 的时候，是否应该 Wrap 这个 error，抛给上层。为什么，应该怎么做请写出代码？
+var (
+	connString = "root@tcp(127.0.0.1:3306)/test"
+	gormClient *gorm.DB
+)
+
+type User struct {
+	Name string
+	Age  int
+}
+
+//从业务逻辑上讲  找不到记录并不是一条错误,但是上层应该知道
+func GetUserList(id string) (User, error) {
+	var user User
+	if err := gormClient.Where("where id = ?", id).Find(user).Error; err == sql.ErrNoRows {
+		return user, err
+	}
+	return user,nil
+
+}
+func init() {
+	//这里省略超时重连等处理
+	db, _ := gorm.Open("mysql", connString)
+	gormClient = db
+}
